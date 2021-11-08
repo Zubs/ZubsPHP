28c12ebb47aadbb97c3f8bffaac98ab01c531692
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-11-09 00:38:25 +0100

    Return route closure

--- Core/Router.php
+++ Core/Router.php
@@ -26,7 +26,14 @@ class Router
     public function resolve()
     {
         $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
 
-        var_dump($path);
+        if ($callback === false) {
+            echo '404 | Not Found';
+            exit();
+        }
+
+        echo call_user_func($callback);
     }
 }
